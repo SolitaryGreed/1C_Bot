48a6a8b6bc800c112ff379e737153be06bc3e6a7
NearPadaZ <padazbze007@gmail.com>
2024-06-30 14:54:30 +0300

    0.2 Validate data

--- 1CBot.py
+++ 1CBot.py
@@ -10,39 +10,61 @@ from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-# Bot token
+# Токен для бота
 TOKEN = config.TOKEN
+url = config.URL
+headers = config.CONTENT_TYPE
 
 # All handlers should be attached to the Router (or Dispatcher)
 dp = Dispatcher()
 
 
+# Повествование бота и команды для отправки
 @dp.message(CommandStart())
 async def command_start_handler(message: Message) -> None:
-    await message.answer("Приветствуем вас в нашем боте! Тут вы можете получить расписание своих занятий.")
+    await message.answer("Приветствуем вас в нашем боте! Тут вы можете получить расписание своих занятий."
+                         "\n\n/schedule - Расписание")
 
 
+# Расписание занятий
 @dp.message(F.text, Command("schedule"))
 async def text_handler(message: Message) -> None:
+    # Создаем словарь с нужными нам значениями для отправки
+    data = await create_data(message)
+    # Отправляем данные через POST и получаем ответ
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=data, headers=headers) as response:
+            if response.status == 200 and str(response.headers["Text"]) == "No register":
+                await message.answer("Вам нужно зарегистрироваться в нашем боте для этого отправьте номер телефона в "
+                                     "формате: +7 (999) 999-99-99")
+            else:
+                await message.answer("Ой что-то пошло не так")
 
-    if not message.from_user.is_bot:
-        url = config.URL
-        headers = {'Content-Type': 'application/json'}
-        data = {
-            "message": message.text,
-            "user_id": message.from_user.id,
-            "username": message.from_user.username,
-            "chat_id": message.chat.id
-        }
 
-        async with aiohttp.ClientSession() as session:
-            async with session.post(url, json=data, headers=headers) as response:
-                if response.status == 200:
-                    await message.reply(str(response.headers["Text"]))
-                else:
-                    await message.reply("Failed to forward message to 1C.")
-    else:
-        return
+# Получения номера телефона
+@dp.message(F.text.regexp(r'^\+\d{1,2}[\s.-]?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{2}[\s.-]?\d{2}$'))
+async def text_handler(message: Message) -> None:
+    # Создаем словарь с нужными нам значениями для отправки
+    data = await create_data(message)
+    # Отправляем данные через POST и получаем ответ
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, json=data, headers=headers) as response:
+            if response.status == 200 and str(response.headers["Text"]) == "Registered":
+                await message.answer("Вы уже зарегистрированы в нашем боте")
+            elif response.status == 200 and str(response.headers["Text"]) == "User is registered":
+                await message.answer("Поздравлю вы зарегистрированы в нашем боте")
+            else:
+                await message.answer("Ой что-то пошло не так")
+
+
+# Формируем словарь с нужными нам значениями для отправки
+async def create_data(message: Message) -> dict:
+    return {
+        "message": message.text,
+        "user_id": message.from_user.id,
+        "username": message.from_user.username,
+        "chat_id": message.chat.id
+    }
 
 
 async def main() -> None:
